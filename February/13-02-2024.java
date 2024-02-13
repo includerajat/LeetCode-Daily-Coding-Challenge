49a8bfe30790bab117567cc084d4fab33b37823b
includerajat <jindalrajat80@gmail.com>
2024-02-13 07:11:49 +0530

    13th February 2024

---
+++ February/13-02-2024.java
@@ -0,0 +1,18 @@
+class Solution {
+  public boolean isPalindrome(String word) {
+    int s = 0, e = word.length() - 1;
+    while (s < e) {
+      if (word.charAt(s++) != word.charAt(e--))
+        return false;
+    }
+    return true;
+  }
+
+  public String firstPalindrome(String[] words) {
+    for (String word : words) {
+      if (isPalindrome(word))
+        return word;
+    }
+    return "";
+  }
+}
